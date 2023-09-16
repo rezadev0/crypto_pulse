f5ed3d8add57aad70041366e7cd8ff4a1f6a26e5
Reza Mohammadi <rezamohammadi8001@gmail.com>
2023-09-16 16:33:25 +0330

    initial app added

---
+++ android/app/src/main/kotlin/com/example/cypto_pulse/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.cypto_pulse
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:cypto_pulse/screens/main_screen.dart';
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Crypto Pulse',
+      home: MainScreen(),
+    );
+  }
+}
