2c707d5d9fc6ef4fd9cfa7ca18867a5a00968c56
Reza Mohammadi <rezamohammadi8001@gmail.com>
2023-09-16 16:54:54 +0330

    status bar color and orientation setting

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
 import 'package:cypto_pulse/screens/main_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 void main(List<String> args) {
+  SystemChrome.setPreferredOrientations(
+    [DeviceOrientation.portraitUp],
+  );
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(
+      statusBarColor: Colors.white,
+      systemNavigationBarColor: Colors.white,
+      systemNavigationBarDividerColor: Colors.white,
+      systemNavigationBarIconBrightness: Brightness.dark,
+      statusBarIconBrightness: Brightness.dark,
+    ),
+  );
   runApp(const MyApp());
 }
 
