3e09d47623ddc3c8d852d6c6bb95f41ae8105730
Reza Mohammadi <rezamohammadi8001@gmail.com>
2024-02-09 17:54:42 +0330

    change a bit DI approach and delete some unused imports

--- lib/data/datasource/coin_datasource.dart
+++ lib/data/datasource/coin_datasource.dart
@@ -1,17 +1,21 @@
-import 'package:cypto_pulse/data/models/coin.dart';
-import 'package:cypto_pulse/getIt/get_it.dart';
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:dio/dio.dart';
+
+import 'package:cypto_pulse/data/models/coin.dart';
 
 abstract class MainCoinDatasource {
   Future<List<Coin>> getCoins();
 }
 
 class CoinDatasource extends MainCoinDatasource {
-  final Dio _dio = getIt.get();
+  final Dio dio ;
+  CoinDatasource({
+    required this.dio,
+  });
   @override
   Future<List<Coin>> getCoins() async {
     try {
-      var response = await _dio.get(
+      var response = await dio.get(
           'markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true');
       List<Coin> coinList = response.data
           .map<Coin>((jsonMapObject) => Coin.fromJson(jsonMapObject))
--- lib/data/repository/coin_repository.dart
+++ lib/data/repository/coin_repository.dart
@@ -1,19 +1,23 @@
-import 'package:cypto_pulse/data/datasource/coin_datasource.dart';
-import 'package:cypto_pulse/data/models/coin.dart';
-import 'package:cypto_pulse/getIt/get_it.dart';
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:dartz/dartz.dart';
 import 'package:dio/dio.dart';
+
+import 'package:cypto_pulse/data/datasource/coin_datasource.dart';
+import 'package:cypto_pulse/data/models/coin.dart';
 
 abstract class MainCoinRepository {
   Future<Either<String, List<Coin>>> getCoins();
 }
 
 class CoinRepository extends MainCoinRepository {
-  final MainCoinDatasource _coinDatasource = getIt.get();
+  final MainCoinDatasource coinDatasource;
+  CoinRepository({
+    required this.coinDatasource,
+  });
   @override
   Future<Either<String, List<Coin>>> getCoins() async {
     try {
-      var response = await _coinDatasource.getCoins();
+      var response = await coinDatasource.getCoins();
       return right(response);
     } on DioException catch (ex) {
       return left(ex.message!);
